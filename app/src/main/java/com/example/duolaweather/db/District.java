a1aea540774612d0b8a222f1975d6bd751da96e8
cjh363508 <18006009246@163.com>
2018-02-02 19:36:02 +0800

    add db tables and config'

---
+++ app/src/main/java/com/example/duolaweather/db/District.java
@@ -0,0 +1,46 @@
+package com.example.duolaweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018/2/2.
+ */
+
+public class District extends DataSupport {
+    private int id;
+    private String name;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
